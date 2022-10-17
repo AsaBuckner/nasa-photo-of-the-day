abf166a9a680fe90effacbbd75e9953bd39c6ac4
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2022-10-17 01:34:00 -0500

    site set up

--- src/ImgComp.js
+++ src/ImgComp.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import "./App.css";
-import ImgComp from "./ImgComp"
+import React from "react";
+import './App.css'
 
-
-  const dummyData = {
+const dummyData = {
     date: "2022-10-17",
 
     explanation : "Why would x-ray rings appear around a gamma-ray burst?  The surprising answer has little to do with the explosion itself but rather with light reflected off areas of dust-laden gas in our own Milky Way Galaxy.  GRB 221009A was a tremendous explosion -- a very bright gamma-ray burst (GRB) that occurred far across the universe with radiation just arriving in our Solar System last week.  Since GRBs can also emit copious amounts of x-rays, a bright flash of x-rays arrived nearly simultaneously with the gamma-radiation. In this case, the X-rays also bounced off regions high in dust right here in our Milky Way Galaxy, creating the unusual reflections. The greater the angle between reflecting Milky Way dust and the GRB, the greater the radius of the X-ray rings, and, typically, the longer it takes for these light-echoes to arrive.",
@@ -13,48 +10,33 @@ import ImgComp from "./ImgComp"
 
     title : "X-Ray Rings Around a Gamma Ray Burst",
 
+    url: "https://apod.nasa.gov/apod/image/2210/GrbRings_SwiftMiller_960.jpg",
   }
 
-function App() {
-  const [Data, setData] = useState(dummyData)
 
+const ImgComp = (props) => {
 
+    return (
+        <div>
 
-
-//  useEffect(() => {
-
-//   axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-
-//     .then(res => { 
-//       setData(res.data)
-//     })
-
-//     .catch(err => {
-//       console.error(err)
-//     })
-
-//     }, [])
-  
-  
-
-
-
-  return (
-
-    <div>
-      <div className="siteheader">
+            <div className="header">
+                <p  className="date" >{props.photo.date}</p>
+                <p className="imgTitle" >{props.photo.title}</p>
+            </div>
         
-          <div className="title">
-            <h1>Daily Cosmos Snapshot</h1>
-          </div>
 
-      </div>
-
-      <ImgComp photo={Data}/>
+            <div className="imageDiv">
+                <img className="image" src={props.photo.hdurl}/>
+            </div>
         
-    </div>
+            
+            <div className="descriptionDiv">
+            <p className="description">{props.photo.explanation}</p>
+            </div>
+        
+        </div>
+    )
 
-  );
 }
 
-export default App;
+export default ImgComp
